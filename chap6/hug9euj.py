215a4f4e84b952f2d148ec609c74c50206337ae9
haruru nakazato <n23020@std.it-college.ac.jp>
2023-05-30 08:02:21 +0000

    git add chap6

---
+++ chap6/hug9euj.py
@@ -0,0 +1,5 @@
+result = ivan = "死の代わりにひとつの光があった。"
+result1 = ivan[0:6]
+result2 = ivan[6:16]
+print(result1)
+print(result2)
---
+++ chap6/zqoscn4.py
@@ -0,0 +1,3 @@
+result = ivan = "死の代わりにひとつの光があった。"
+result1 = ivan[6:]
+print(result1)
